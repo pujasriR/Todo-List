54f1921fe10f323b985816aba8e8238f346b0e73
pujasri <raminenichinnisai@gmail.com>
2024-06-16 15:53:08 -0500

    firsttodo

---
+++ src/components/Todo.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef, useState } from 'react'
+import './CSS/Todo.css'
+import { TodoItems } from './TodoItems';
+let count=0;
+export const Todo = () => {
+const [todos,setTodos]=useState([]);
+const inputRef=useRef(null);
+const add=()=>{
+    setTodos([...todos,{no:count++,text:inputRef.current.value,display:""}])
+    inputRef.current.value="";
+}
+
+useEffect(()=>{
+    setTodos(JSON.parse(localStorage.getItem("todos")));
+},[])
+
+useEffect(()=>{
+    setTimeout(()=>{console.log(todos);
+        localStorage.setItem("todos",JSON.stringify(todos));
+    },100);
+    
+
+},[todos])
+
+
+  return (
+    <div className="todo">
+        <div className="todo-header"> To-Do List</div>
+        <div className="todo-add">
+            <input ref={inputRef} type="text" placeholder="Add your task" className="todo-input"/>
+            <div onClick={()=>{add()}} className="todo-add-btn">ADD</div>
+        </div>
+        <div className="todo-list">
+            {todos.map((item,index)=>{
+                return <TodoItems key={index} setTodos={setTodos} no={item.no} display={item.display} text={item.text}/>
+
+            })}
+        </div>
+    </div>
+  )
+}
---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,39 @@
+import './CSS/TodoItems.css'
+import checkmark from './Assets/checkmark.jpeg'
+import crossmark from './Assets/crossmark.jpeg'
+import uncheck from './Assets/uncheck.png'
+
+export const TodoItems = ({no,display,text,setTodos}) => {
+    const deletes = (no)=>{
+        let data=JSON.parse(localStorage.getItem("todos"));
+        data=data.filter((todo)=>todo.no!==no)
+        setTodos(data);
+    }
+    
+    const toggle=(no)=>{
+        let data=JSON.parse(localStorage.getItem("todos"));
+        for(let i=0;i<data.length;i++){
+            if(data[i].no==no){
+                if(data[i].display===""){
+                    data[i].display="line-through";
+
+                }
+                else{
+                    data[i].display="";
+                }
+                break;
+            }
+        }
+        setTodos(data);
+    }
+  return (
+    <div className='todo-items'>
+        <div className='todoitems-container' onClick={()=>{toggle(no)}}>
+            {display===""?<img src={uncheck} alt=""width="50" height="60"/>:<img src={checkmark} alt=""width="50" height="60"/>}
+            
+            <div className='todoitems-text'>{text}</div>
+            </div>
+        <img className="todoitems-cross-icon" onClick={()=>{deletes(no)}} src={crossmark} alt=""width="40" height="50"/>
+    </div>
+  )
+}
